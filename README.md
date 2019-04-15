a01846154f0b5d62896ae164ae0717f7c5aa6223
donghaichen <chendonghai888@gmail.com>
2019-04-15 16:14:52 +0800

    test

---
+++ README.md
@@ -0,0 +1,7 @@
+# TradingView  Vue.js demo with websocket
+
+## How to start
+
+1. Install dependencies `npm install`.
+
+1. Run `npm run serve`. 
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <trade-view ref="trade"></trade-view>
+  </div>
+</template>
+
+<script>
+import TradeView from './components/TradeView'
+export default {
+  name: 'app',
+  components: {
+    TradeView
+  },
+  mounted() {
+    this.$refs.trade.init('BTCUSDT', 5)
+  }
+}
+</script>
+
+<style scoped>
+  .app {
+    text-align: center;
+  }
+  .app .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 0;
+    background-color: #222;
+    color: #fff;
+  }
+  .app .title {
+    display: block;
+    font-size: 1.5em;
+  }
+</style>
+
+
